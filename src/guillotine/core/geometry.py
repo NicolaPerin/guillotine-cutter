0af1eb07d2ff6b9f13d2924b60a665b44e801180
Nicola Perin <nicolaperin1998@gmail.com>
2026-01-27 22:19:31 +0100

    Store defects as tuples to pass single defect test
    Added defects list with (x, y, w, h, x_end, y_end) tuples

--- src/guillotine/core/geometry.py
+++ src/guillotine/core/geometry.py
@@ -10,6 +10,15 @@ class SheetGeometry:
         """Initialize geometry with sheet size and defects."""
         self.W0, self.H0 = sheet_sz
         
-        # Count defects
+        # Store defects as list of tuples: (x, y, w, h, x_end, y_end)
+        self.defects = []
         n_def = len(defect_sizes[0]) if len(defect_sizes) > 0 and len(defect_sizes[0]) > 0 else 0
+        
+        for i in range(n_def):
+            dx = int(defect_positions[0][i])
+            dy = int(defect_positions[1][i])
+            dw = int(defect_sizes[0][i])
+            dh = int(defect_sizes[1][i])
+            self.defects.append((dx, dy, dw, dh, dx + dw, dy + dh))
+        
         self.n_def = n_def
