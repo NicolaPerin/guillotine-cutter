156d652007fb00ddafbea0677bf3ad28525a8c6d
Nicola Perin <nicolaperin1998@gmail.com>
2026-01-27 22:21:00 +0100

    Add test: is_pure with no defects

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -37,3 +37,15 @@ def test_init_with_single_defect():
     assert dh == 2
     assert dx_end == 7
     assert dy_end == 7
+
+def test_is_pure_no_defects():
+    """With no defects, all rectangles should be pure."""
+    geom = SheetGeometry(
+        sheet_sz=(20, 20),
+        defect_sizes=[[], []],
+        defect_positions=[[], []]
+    )
+    
+    assert geom.is_pure(0, 0, 10, 10) is True
+    assert geom.is_pure(5, 5, 5, 5) is True
+    assert geom.is_pure(0, 0, 20, 20) is True
