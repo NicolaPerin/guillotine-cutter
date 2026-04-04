5068990ce9ed2552287765c37f8b146cafe33a32
Nicola Perin <nicolaperin1998@gmail.com>
2026-04-04 18:57:44 +0200

    remove any diff from feature branch setup.py and main one

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 import numpy as np
-
+ 
 ext = Extension(
     name="guillotine.core._solver",
     sources=[
@@ -9,10 +9,10 @@ ext = Extension(
     ],
     include_dirs=[
         np.get_include(),
-        "src/guillotine/core",
+        "src/guillotine/core",    # for #include "solver_core.h"
     ],
     extra_compile_args=["-O2", "-funroll-loops", "-fopenmp", "-Wall"],
     extra_link_args=["-fopenmp"],
 )
-
+ 
 setup(ext_modules=[ext])
