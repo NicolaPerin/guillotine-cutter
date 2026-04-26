e3a84e042f01b970c49f355e2601f2bf97af463f
Nicola Perin <nicolaperin1998@gmail.com>
2026-04-27 00:38:20 +0200

    build: explicitly declare src/ layout in setup.py
    Newer setuptools infers the src/ layout automatically, but older versions
    do not. Adding package_dir={: src} makes the --inplace copy target
    src/guillotine/core/ correctly on all setuptools versions.

--- setup.py
+++ setup.py
@@ -9,18 +9,17 @@ ext = Extension(
     ],
     include_dirs=[
         np.get_include(),
-        "src/guillotine/core",    # for #include "solver_core.h"
+        "src/guillotine/core",
     ],
     extra_objects=['build/gpu_solver.o'],
-    
-    # --- ADDED CUDA LINKING ARGS ---
-    library_dirs=['/usr/local/cuda/lib64'],          # Build-time path to libcudart.so
-    libraries=['cudart'],                            # Tells the linker to use libcudart
-    runtime_library_dirs=['/usr/local/cuda/lib64'],  # Bakes the path into the .so for runtime (RPATH)
-    # -------------------------------
-    
+    library_dirs=['/usr/local/cuda/lib64'],
+    libraries=['cudart'],
+    runtime_library_dirs=['/usr/local/cuda/lib64'],
     extra_compile_args=["-O3", "-march=native", "-fopenmp", "-Wall", "-Wextra"],
     extra_link_args=["-fopenmp"],
 )
 
-setup(ext_modules=[ext])
+setup(
+    package_dir={"": "src"},
+    ext_modules=[ext],
+)
